9b62db04dcb437b1d3f03f82bcb898bb33df9e80
WhiteBlueBird <76052816+WhiteBlueParrot@users.noreply.github.com>
2025-03-01 20:40:09 +0300

    initial commit

---
+++ NewsPortal/news/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    rating = models.IntegerField(default=0)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def update_rating(self):
+        self.rating = sum(post.rating * 3 for post in self.posts.all())
+        self.rating += sum(comment.rating for comment in self.user.comment_set.all())
+        for post in self.posts.all():
+            for comment in post.comment_set.all():
+                self.rating += comment.rating
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Post(models.Model):
+    ARTICLE = 'AR'
+    NEWS = 'NW'
+    POST_TYPES = [
+        (ARTICLE, 'статья'),
+        (NEWS, 'новость'),
+    ]
+
+    post_type = models.CharField(max_length=2, choices=POST_TYPES, default=ARTICLE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
+    categories = models.ManyToManyField(Category, through='PostCategory')
+
+    def preview(self):
+        return self.text[:124] + '...'
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
