b6a7bfbe612f759b14dd9e1c454c09d18882f27b
WhiteBlueBird <76052816+WhiteBlueParrot@users.noreply.github.com>
2025-03-20 19:26:55 +0300

    add groups

--- NewsPortal/sign/models.py
+++ NewsPortal/sign/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
+
 
 class BaseRegisterForm(UserCreationForm):
     email = forms.EmailField(label="Email")
@@ -17,3 +20,12 @@ class BaseRegisterForm(UserCreationForm):
                   "email",
                   "password1",
                   "password2",)
+
+
+class CommonSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(CommonSignupForm, self).save(request)
+        common_group = Group.objects.get(name='common')
+        common_group.user_set.add(user)
+        return user
--- NewsPortal/sign/urls.py
+++ NewsPortal/sign/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import BaseRegisterView
+from .views import BaseRegisterView, become_author
 
 urlpatterns = [
     path('login/',
@@ -12,4 +12,5 @@ urlpatterns = [
     path('signup/',
          BaseRegisterView.as_view(template_name='sign/signup.html'),
          name='signup'),
+    path('become_author/', become_author, name='become_author'),
 ]
