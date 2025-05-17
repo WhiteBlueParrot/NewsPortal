ed0bc6830b47d6872a1a111cad82139e7c8a6b3d
WhiteBlueBird <76052816+WhiteBlueParrot@users.noreply.github.com>
2025-05-17 16:54:04 +0300

    add logging

--- NewsPortal/news/templatetags/custom_filters.py
+++ NewsPortal/news/templatetags/custom_filters.py
@@ -17,3 +17,17 @@ def censor(text):
 
     pattern = r'\b(' + '|'.join(re.escape(word) for word in PROFANITY) + r')\b'
     return re.sub(pattern, replace, text, flags=re.IGNORECASE)
+
+
+# @register.filter()
+# def hide_forbidden(value):
+#     words = value.split()
+#     result = []
+#     for word in words:
+#         if word in forbidden_words:
+#             result.append(word[0] + ((len(word) - 2) * '*') + word[-1])
+#         else:
+#             result.append(word)
+#     return ''.join(result)
+
+
